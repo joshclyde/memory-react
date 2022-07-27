432b1f95886c6c86a03505abb2de21536ea9edb0
Josh Clyde <joshclyde11@gmail.com>
2022-07-27 14:25:29 -0400

    Add alias for src

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
+import { Bar } from "@/Bar";
+
 import "./App.css";
 
 export const App = () => {
@@ -8,24 +9,7 @@ export const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Bar />
     </div>
   );
 };
---
+++ src/Bar/index.tsx
@@ -0,0 +1,3 @@
+export const Bar = () => {
+  return <div>Hello there</div>;
+};
