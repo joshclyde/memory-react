4cd76d7fb1429c14f86cd11c021a01a0800c7320
Josh Clyde <joshclyde11@gmail.com>
2022-07-27 15:42:46 -0400

    Add an alias for components

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@Components": path.resolve(__dirname, "./src/Components"),
     },
   },
 });
