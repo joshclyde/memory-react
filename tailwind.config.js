b7a927a8cf0d8e23f78ac7c4b1451039993cd135
Josh Clyde <joshclyde11@gmail.com>
2022-07-27 21:10:36 -0400

    Add colors and Lato font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-1": "#232530",
+      "dark-2": "#353846",
+      "light-1": "#f8f8f2",
+      "light-2": "#BFBFC3",
+      "green-1": "#57E889",
+      "purple-1": "#CDAEF9",
+      "blue-1": "#86E2F5",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
