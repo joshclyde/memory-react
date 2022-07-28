067362a30e7476772ff7644178ae39ea9b940a55
Josh Clyde <joshclyde11@gmail.com>
2022-07-28 11:44:53 -0400

    Move search bar into ListOfMemories

--- src/Components/ListOfMemories/index.tsx
+++ src/Components/ListOfMemories/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { HiPlus } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 import { state } from "@Components/state";
@@ -5,17 +7,34 @@ import { state } from "@Components/state";
 import { Item } from "./Item";
 
 export const ListOfMemories = () => {
+  const [searchTerm, setSearchTerm] = useState(``);
+
   return (
-    <div className="divide-y overflow-y-scroll">
-      {Object.entries(state.flashcards.flashcardsIncludingDeleted).map(([id, props]) => {
-        return (
-          <div key={id} className="border-dark-1">
-            <Link to={`/memories/${id}`}>
-              <Item {...props} />
-            </Link>
-          </div>
-        );
-      })}
+    <div className="basis-96 shrink-0 bg-dark-2 border-r border-dark-1 h-screen flex flex-col">
+      <div className="flex p-2 items-center border-dark-1 border-b">
+        <input
+          className="border border-black grow bg-dark-2 border-blue-1 rounded-lg text-blue-1 pl-2 text-base"
+          type="text"
+          value={searchTerm}
+          onChange={(x) => setSearchTerm(x.target.value)}
+        />
+        <Link to="/memories" className="ml-2 text-blue-1 border border-blue-1 rounded">
+          <HiPlus />
+        </Link>
+      </div>
+      <div className="divide-y overflow-y-scroll">
+        {Object.entries(state.flashcards.flashcardsIncludingDeleted).map(
+          ([id, props]) => {
+            return (
+              <div key={id} className="border-dark-1">
+                <Link to={`/memories/${id}`}>
+                  <Item {...props} />
+                </Link>
+              </div>
+            );
+          },
+        )}
+      </div>
     </div>
   );
 };
--- src/Components/Routes/Memories/index.tsx
+++ src/Components/Routes/Memories/index.tsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
-import { HiPlus } from "react-icons/hi";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { ListOfMemories } from "@Components/ListOfMemories";
 
 export const Memories = () => {
-  const [searchTerm, setSearchTerm] = useState(``);
-
   return (
     <>
-      <div className="basis-96 shrink-0 bg-dark-2 border-r border-dark-1 h-screen flex flex-col">
-        <div className="flex p-2 items-center border-dark-1 border-b">
-          <input
-            className="border border-black grow bg-dark-2 border-blue-1 rounded-lg text-blue-1 pl-2 text-base"
-            type="text"
-            value={searchTerm}
-            onChange={(x) => setSearchTerm(x.target.value)}
-          />
-          <Link to="/memories" className="ml-2 text-blue-1 border border-blue-1 rounded">
-            <HiPlus />
-          </Link>
-        </div>
-        <ListOfMemories />
-      </div>
+      <ListOfMemories />
       <div className="bg-dark-2 w-full">
         <Outlet />
       </div>
