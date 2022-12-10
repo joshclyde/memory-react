20a662142159badc8cb23748a50287c6fafe8e83
Josh Clyde <joshclyde11@gmail.com>
2022-12-10 10:49:25 -0500

    Create new memory page

--- src/components/Routes/Memories/NewMemory/index.tsx
+++ src/components/Routes/Memories/NewMemory/index.tsx
@@ -1,3 +1,88 @@
+import { useState } from "react";
+import { HiCheck, HiX } from "react-icons/hi";
+
+import { Checkbox } from "src/components/Design/Checkbox";
+import { TextArea } from "src/components/Design/TextArea";
+import { useAppDispatch, useAppSelector } from "src/store";
+import { createFlashcard } from "src/store/flashcardsSlice";
+import { useTagsFormOptions } from "src/store/selectors";
+import { StateFlashcard } from "src/store/types";
+
+import { ActionsView, BodyView, WholeView } from "../ExistingMemory/core";
+
 export const NewMemory = () => {
-  return <div>New Memory</div>;
+  const [front, setFront] = useState(``);
+  const [back, setBack] = useState(``);
+  const [tags, setTags] = useState<Record<string, boolean>>({});
+  const [status, setStatus] = useState<null | "PENDING" | "ERROR" | "SUCCESS">(null);
+  const dispatch = useAppDispatch();
+  const tagsFormOptions = useTagsFormOptions();
+
+  const save = async () => {
+    setStatus(`PENDING`);
+    try {
+      await dispatch(
+        createFlashcard({
+          data: {
+            front,
+            back,
+            tags: Object.entries(tags)
+              .filter(([_id, checked]) => checked)
+              .map(([id]) => id),
+          },
+        }),
+      ).unwrap();
+      setStatus(`SUCCESS`);
+      setFront(``)
+      setBack(``)
+    } catch (rejectedValueOrSerializedError) {
+      setStatus(`ERROR`);
+    }
+  };
+
+  return (
+    <WholeView>
+      <BodyView>
+        <div className="flex flex-col">
+          <fieldset className="flex flex-wrap justify-between gap-2">
+            {tagsFormOptions.map(({ id, name }) => {
+              return (
+                <Checkbox
+                  key={id}
+                  id={id}
+                  name={name}
+                  checked={Boolean(tags[id])}
+                  onChange={(e) =>
+                    setTags((prev) => ({ ...prev, [id]: e.target.checked }))
+                  }
+                />
+              );
+            })}
+          </fieldset>
+          <TextArea
+            className="mt-4"
+            value={front}
+            onChange={(event) => setFront(event.target.value)}
+          />
+          <TextArea
+            className="mt-4"
+            value={back}
+            onChange={(event) => setBack(event.target.value)}
+          />
+        </div>
+        {status === `PENDING` && `Submitting...`}
+        {status === `ERROR` && `Failed to save.`}
+      </BodyView>
+      <ActionsView>
+        <button
+          title="Save Changes"
+          type="button"
+          className="mt-4"
+          onClick={() => save()}
+        >
+          <HiCheck size="2em" />
+        </button>
+      </ActionsView>
+    </WholeView>
+  );
 };
