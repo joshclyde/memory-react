bccee1875795d57091b8abc16d597ad802758645
Josh Clyde <joshclyde11@gmail.com>
2023-01-03 19:38:32 -0500

    Add rollup-plugin-visualizer to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
+
 import react from "@vitejs/plugin-react";
 // const path = require("path");
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ open: true, template: "sunburst" }) as PluginOption],
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
